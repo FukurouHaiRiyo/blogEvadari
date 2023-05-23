9b848c10fd076d6f596e4a5608193b7d3515ffee
FukurouHaiRiyo <andreipanait00@gmail.com>
2023-05-23 16:40:10 +0300

    Added Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='text-white py-4 px-6'>
+      <p className='text-center'>&copy; 2020 Stirirapide.net. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
