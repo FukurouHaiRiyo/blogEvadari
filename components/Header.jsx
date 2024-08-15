7471c734352b956f081218bce908dd7ba3b768f9
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-08-15 13:13:46 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
             <div className='md:float-left block'>
                   <Link href='/'>
                         <span className='cursor-pointer font-bold text-4xl text-white'>
-                              Stiri Rapide
+                              Evadări Perspicace
                         </span>
                   </Link>
             </div>
